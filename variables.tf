17ea181b4e6e86d7d5c5d658c38e83f798e31ad8
tprabhaker <tprabhaker@gmail.com>
2025-05-12 17:37:56 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -9,3 +9,13 @@ variable "latency_threshold" {
   variable "http_500_threshold" {
     default = "5"
   }
+
+variable "cpu_usage_msg" {
+    default = "Critical CPU usage alert!"
+  }
+variable "latency_msg" {
+    default = "Critical latency alert"
+  }
+variable "http_500_msg" {
+    default = "Critical 500 errors alert"
+  }
